8ac3f2bad991a24f4fdd0fbf5db7303f50ea9acb
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-09-23 14:50:17 +0200

    Solve 'Majority Element'

---
+++ 169. Majority Element/solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+  let candidate = null;
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (count === 0) {
+      candidate = nums[i];
+    }
+
+    count += candidate === nums[i] ? 1 : -1;
+  }
+
+  return candidate;
+};
