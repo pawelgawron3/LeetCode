02581c74e10b9fda2f01c36a2e0fcc2c82b5d084
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-09-23 20:08:23 +0200

    Use j instead of i

--- 26. Remove Duplicates from Sorted Array/solution.cs
+++ 26. Remove Duplicates from Sorted Array/solution.cs
@@ -3,7 +3,7 @@ public class Solution {
         int j = 1;
 
         for(int i = 1; i < nums.Length; i++){
-            if(nums[i] != nums[i-1]){
+            if(nums[i] != nums[j-1]){
                 nums[j] = nums[i];
                 j++;
             }
--- 26. Remove Duplicates from Sorted Array/solution.js
+++ 26. Remove Duplicates from Sorted Array/solution.js
@@ -6,7 +6,7 @@ var removeDuplicates = function (nums) {
   let j = 1;
 
   for (let i = 1; i < nums.length; i++) {
-    if (nums[i] !== nums[i - 1]) {
+    if (nums[i] !== nums[j - 1]) {
       nums[j] = nums[i];
       j++;
     }
